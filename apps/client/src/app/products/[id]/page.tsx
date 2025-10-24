80f62ac760927b4d11c8500fe0cf8fc0f6623ecd
Aslonov Davronbek <davronaslonov01@gmail.com>
2025-10-24 15:23:41 +0500

    bugs fixed

--- apps/client/src/app/products/[id]/page.tsx
+++ apps/client/src/app/products/[id]/page.tsx
@@ -1,99 +1,99 @@
-// import ProductInteraction from "@/components/ProductInteraction";
-// import { ProductType } from "@/types";
-// import Image from "next/image";
-// import React from "react";
-// // interface ProductPageProps {
-// //   params: { id: string };
-// //   searchParams: Record<string, string | string[] | undefined>;
-// // }
-// type Params = { id: string };
-// type SearchParams = { [key: string]: string | string[] | undefined };
-// const fetchdata = async (id: string) => {
-//   const res = await fetch(
-//     `${process.env.NEXT_PUBLIC_PRODUCT_SERVICE_URL}/products/${id}`
-//   );
+import ProductInteraction from "@/components/ProductInteraction";
+import { ProductType } from "@/types";
+import Image from "next/image";
+import React from "react";
+interface ProductPageProps {
+  params: { id: string };
+  searchParams: Record<string, string | string[] | undefined>;
+}
+type Params = { id: string };
+type SearchParams = { [key: string]: string | string[] | undefined };
+const fetchdata = async (id: string) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_PRODUCT_SERVICE_URL}/products/${id}`
+  );
 
-//   if (!res.ok) {
-//     throw new Error("Prodoct not");
-//   }
+  if (!res.ok) {
+    throw new Error("Prodoct not");
+  }
 
-//   const data: ProductType = await res.json();
-//   return data;
-// };
+  const data: ProductType = await res.json();
+  return data;
+};
 
-// const ProductPage = async ({
-//   params,
-//   searchParams,
-// }: {
-//   params: Params;
-//   searchParams: SearchParams;
-// }) => {
-//   const { id } = params;
-//   const { color, size } = searchParams;
+const ProductPage = async ({
+  params,
+  searchParams,
+}: {
+  params: Params;
+  searchParams: SearchParams;
+}) => {
+  const { id } = params;
+  const { color, size } = searchParams;
 
-//   const product = await fetchdata(id);
+  const product = await fetchdata(id);
 
-//   const selectedSize = size || product.sizes?.[0] || "Default Size";
-//   const selectedColor = color || product.colors?.[0] || "Default Color";
+  const selectedSize = size || product.sizes?.[0] || "Default Size";
+  const selectedColor = color || product.colors?.[0] || "Default Color";
 
-//   return (
-//     <div className="w-full flex flex-col lg:flex-row mt-12 gap-12">
-//       <div className="w-full lg:w-5/12 relative aspect-[2/3]">
-//         {product.images?.[selectedColor] && (
-//           <Image
-//             alt={product.name}
-//             className="object-contain rounded-md"
-//             src={product.images[selectedColor]}
-//             fill
-//           />
-//         )}
-//       </div>
-//       <div className="w-full lg:w-7/12 flex flex-col gap-4">
-//         <h1 className="text-xl font-medium">{product.name}</h1>
-//         <p className="text-gray-500">{product.description}</p>
-//         <h2 className="text-2xl font-semibold">${product.price.toFixed(2)}</h2>
+  return (
+    <div className="w-full flex flex-col lg:flex-row mt-12 gap-12">
+      <div className="w-full lg:w-5/12 relative aspect-[2/3]">
+        {product.images?.[selectedColor] && (
+          <Image
+            alt={product.name}
+            className="object-contain rounded-md"
+            src={product.images[selectedColor]}
+            fill
+          />
+        )}
+      </div>
+      <div className="w-full lg:w-7/12 flex flex-col gap-4">
+        <h1 className="text-xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
+        <h2 className="text-2xl font-semibold">${product.price.toFixed(2)}</h2>
 
-//         <ProductInteraction
-//           product={product}
-//           selectedSize={selectedSize}
-//           selectedColor={selectedColor}
-//         />
+        <ProductInteraction
+          product={product}
+          selectedSize={selectedSize}
+          selectedColor={selectedColor}
+        />
 
-//         <div className="flex items-center gap-2 mt-4">
-//           <Image
-//             src="/klarna.png"
-//             alt="klarna"
-//             width={50}
-//             height={25}
-//             className="rounded-md"
-//           />
-//           <Image
-//             src="/cards.png"
-//             alt="cards"
-//             width={50}
-//             height={25}
-//             className="rounded-md"
-//           />
-//           <Image
-//             src="/stripe.png"
-//             alt="stripe"
-//             width={50}
-//             height={25}
-//             className="rounded-md"
-//           />
-//         </div>
+        <div className="flex items-center gap-2 mt-4">
+          <Image
+            src="/klarna.png"
+            alt="klarna"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/cards.png"
+            alt="cards"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/stripe.png"
+            alt="stripe"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+        </div>
 
-//         <p className="text-gray-500 text-xs mt-2">
-//           By clicking Pay Now, you agree to our{" "}
-//           <span className="underline hover:text-black">Terms & Conditions</span>{" "}
-//           and <span className="underline hover:text-black">Privacy Policy</span>
-//           . You authorize us to charge your selected payment method for the
-//           total amount shown. All sales are subject to our return and{" "}
-//           <span className="underline hover:text-black">Refund Policies</span>.
-//         </p>
-//       </div>
-//     </div>
-//   );
-// };
+        <p className="text-gray-500 text-xs mt-2">
+          By clicking Pay Now, you agree to our{" "}
+          <span className="underline hover:text-black">Terms & Conditions</span>{" "}
+          and <span className="underline hover:text-black">Privacy Policy</span>
+          . You authorize us to charge your selected payment method for the
+          total amount shown. All sales are subject to our return and{" "}
+          <span className="underline hover:text-black">Refund Policies</span>.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-// export default ProductPage;
+export default ProductPage;
